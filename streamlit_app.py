257854c86bed37f4528ff79915b2e528e7676fc5
js-lim-laiv <jslim@laivdata.com>
2025-07-17 00:05:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 
 # Hugging Face Inference API 설정
 HF_TOKEN = st.secrets["HF_TOKEN"]  # secrets.toml에 등록 필요
-API_URL = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct"
+API_URL = "https://api-inference.huggingface.co/models/OpenAssistant/oasst-sft-1-pythia-12b"
 headers = {"Authorization": f"Bearer {HF_TOKEN}"}
 
 # Streamlit UI
